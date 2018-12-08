b0f4727d371439c320f6c3532f3c060b7ca7b13b
Student <student@Admins-MacBook-Air-5.local>
2018-12-08 15:41:13 -0800

    Initial commit

---
+++ app/src/main/java/com/teamedge/android/pollogram/MainActivity.java
@@ -0,0 +1,25 @@
+package com.teamedge.android.pollogram;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    int likes = 0;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }/**
+     * This method displays the likes on the screen.
+     */
+    private void displayLikes(int number) {
+        TextView likesTextView = (TextView) findViewById(R.id.likes_text_view);
+        likesTextView.setText("Likes: " + number);
+    }
+    public void increase(View view) {
+        likes++;
+        displayLikes(likes);
+    }
+}
